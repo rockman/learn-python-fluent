d0777dd6f5466eaa7eb8ff2f489d12bd0fd7c83a
rockman <rockman@users.noreply.github.com>
2020-01-28 19:55:34 +1100

    Finished Chapter 4

--- chp04/test_chp04.py
+++ chp04/test_chp04.py
@@ -16,3 +16,12 @@ def test_constructing_bytes():
     short_array = array.array('h', [-1, 0, 1])
     assert bytes(byte_array) == b'\xff\x00\x01'
     assert bytes(short_array) == b'\xff\xff\x00\x00\x01\x00'
+
+
+def test_bytes_regex():
+    import re
+
+    tamil_numbers = '\u0bed\u0bee'
+
+    assert len(re.findall(rb'\d', tamil_numbers.encode('utf-8'))) == 0
+    assert len(re.findall(r'\d', tamil_numbers)) == 2
